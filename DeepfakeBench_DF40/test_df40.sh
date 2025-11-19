590336d04e618d125711dc223ff1e5af8a005ab9
Ant-One <antoine.moix@epfl.ch>
2025-11-20 00:09:28 +0100

    More results again

--- DeepfakeBench_DF40/test_df40.sh
+++ DeepfakeBench_DF40/test_df40.sh
@@ -9,6 +9,26 @@ conda activate df40
 # python training/test.py --detector_path training/config/detector/clip_base.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_df40/clip.pth
 # python training/test.py --detector_path training/config/detector/clip_base.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_df40/clip.pth
 
-python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
-python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
-python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
+# python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
+# python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
+# python training/test.py --detector_path training/config/detector/i3d.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_df40/i3d.pth
+
+# python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_fs/spsl.pth
+# python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_fr/spsl.pth
+# python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_efs/spsl.pth
+
+# python training/test.py --detector_path training/config/detector/recce.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_fs/recce.pth
+# python training/test.py --detector_path training/config/detector/recce.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_fr/recce.pth
+# python training/test.py --detector_path training/config/detector/recce.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_efs/recce.pth
+
+# python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_fs/sbi.pth
+# python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_fr/sbi.pth
+# python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_efs/sbi.pth
+
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_fs/srm.pth
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_fr/srm.pth
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_efs/srm.pth
+
+python training/test.py --detector_path training/config/detector/rfm.yaml --test_dataset FSAll_cdf --weights_path training/df40_weights/train_on_fs/rfm.pth
+python training/test.py --detector_path training/config/detector/rfm.yaml --test_dataset FRAll_cdf --weights_path training/df40_weights/train_on_fr/rfm.pth
+python training/test.py --detector_path training/config/detector/rfm.yaml --test_dataset EFSAll_cdf --weights_path training/df40_weights/train_on_efs/rfm.pth
