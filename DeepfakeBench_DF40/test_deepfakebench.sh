25bdc5a7130a5662af20d1e49b4c94d342637188
Ant-One <antoine.moix@epfl.ch>
2026-01-05 13:56:13 +0100

    Results on ConfDF

--- DeepfakeBench_DF40/test_deepfakebench.sh
+++ DeepfakeBench_DF40/test_deepfakebench.sh
@@ -1,6 +1,23 @@
 eval "$(conda shell.zsh hook)"
 conda activate df40
 
+python training/test.py --detector_path training/config/detector/exception.yaml --test_dataset FSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/exception_best.pth
+python training/test.py --detector_path training/config/detector/exception.yaml --test_dataset FRAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/exception_best.pth
+python training/test.py --detector_path training/config/detector/exception.yaml --test_dataset EFSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/exception_best.pth
+
+
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset FSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/srm_best.pth
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset FRAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/srm_best.pth
+python training/test.py --detector_path training/config/detector/srm.yaml --test_dataset EFSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/srm_best.pth
+
+python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset FSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/spsl_best.pth
+python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset FRAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/spsl_best.pth
+python training/test.py --detector_path training/config/detector/spsl.yaml --test_dataset EFSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/spsl_best.pth
+
+python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset FSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
+python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset FRAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
+python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset EFSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
+
 # python training/test.py --detector_path training/config/detector/sbi.yaml --test_dataset FSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
 # python training/test.py --detector_path training/config/detector/clip_large.yaml --test_dataset FRAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
 # python training/test.py --detector_path training/config/detector/clip_large.yaml --test_dataset EFSAll_cdf --weights_path training/deepfakebench_weights/train_on_ff-orig/sbi_best.pth
